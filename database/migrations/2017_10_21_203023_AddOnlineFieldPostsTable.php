865d0fcdb44494db52d97ed07ed56edd8ea067ae
Benoît Mallo <benoit.mallo@gmail.com>
2017-10-22 07:51:19 +0000

    ADD Delete the 'online' field in the migration.

--- database/migrations/2017_10_21_203023_AddOnlineFieldPostsTable.php
+++ database/migrations/2017_10_21_203023_AddOnlineFieldPostsTable.php
@@ -26,7 +26,7 @@ class AddOnlineFieldPostsTable extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->dropColumn('online');
         });
     }
 }
