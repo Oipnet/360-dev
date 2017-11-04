a43b26a03e17407d8ab883ae4a0baa7910b339fb
Benoît Mallo <benoit.mallo@gmail.com>
2017-11-04 10:53:50 +0000

    FIX #15 Delete materialize js

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -197,13 +197,7 @@
 
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
-<script type="text/javascript">
-  $(document).ready(function() {
-    $('.button-collapse').sideNav();
-    $('.collapsible').collapsible();
-    $('select').material_select();
-  });
-</script>
+
 <script type="text/javascript" src="/js/app.js"></script>
 
 </body>
