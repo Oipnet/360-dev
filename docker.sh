d0134bf12399f29bcfa66aa02c875f406fd506f5
mosleymos <keitamori@gmail.com>
2017-11-08 20:33:59 +0000

    Meilleur script

--- docker.sh
+++ docker.sh
@@ -7,42 +7,6 @@ php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece38
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
-export APP_NAME=Laravel
-export APP_ENV=local
-export APP_KEY=''
-export APP_DEBUG=true
-export APP_LOG_LEVEL=debug
-export APP_URL=http://localhost
-
-export DB_CONNECTION=mysql
-export DB_HOST=127.0.0.1
-export DB_PORT=3306
-export DB_DATABASE=homestead
-export DB_USERNAME=homestead
-export DB_PASSWORD=secret
-
-export BROADCAST_DRIVER=log
-export CACHE_DRIVER=file
-export SESSION_DRIVER=file
-export QUEUE_DRIVER=sync
-
-export REDIS_HOST=127.0.0.1
-export REDIS_PASSWORD=null
-export REDIS_PORT=6379
-
-export MAIL_DRIVER=smtp
-export MAIL_HOST=smtp.mailtrap.io
-export MAIL_PORT=2525
-export MAIL_USERNAME=null
-export MAIL_PASSWORD=null
-export MAIL_ENCRYPTION=null
-
-export PUSHER_APP_ID=''
-export PUSHER_APP_KEY=''
-export PUSHER_APP_SECRET=''
-
 apt-get update
 apt-get install -y unzip 
-DEBIAN_FRONTEND=noninteractive apt-get -y install mysql-server
 php composer.phar install
-mysqld &
