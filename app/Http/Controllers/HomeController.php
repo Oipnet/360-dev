99290f0a705a7a72b28a8af15564dc1bda837f45
Benoît Mallo <benoit.mallo@gmail.com>
2017-11-12 08:09:11 +0000

    FIX #10 Delete auth middleware for Home access

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,28 +1,21 @@
 <?php
-
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
+/**
+ * HomeController
+ */
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
     /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(): Response
     {
-        return view('home');
+        return response()->view('home');
     }
 }
