aabe8f173c41890b9c2b1e4c7c0730242be3770a
TuxBoy <benoit.mallo@gmail.com>
2018-09-09 16:59:49 +0200

    IMP Edit/delete role

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -54,17 +54,6 @@ class RoleController extends AdminController
 		return redirect()->back();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
 	/**
 	 * Show the form for editing the specified resource.
 	 *
